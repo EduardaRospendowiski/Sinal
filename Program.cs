50ea565137ab39914fd18fafffb8d0dc508414c2
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 20:43:59 -0300

    atividade completa

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+ 
+int numero;
+ConsoleColor cor;
+ 
+Console.WriteLine("--- Sinal ---");
+ 
+Console.Write("Digite um numero: ");
+numero = Convert.ToInt32(Console.ReadLine());
+ 
+if (numero > 0)
+{
+    cor = ConsoleColor.Blue;
+    Console.ForegroundColor = cor;
+    Console.WriteLine("Positivo");
+    Console.ResetColor();
+}
+ 
+else if (numero == 0)
+{
+    cor = ConsoleColor.Magenta;
+    Console.ForegroundColor = cor;
+    Console.WriteLine("Zero");
+    Console.ResetColor();
+}
+ 
+else
+{
+    cor = ConsoleColor.DarkRed;
+    Console.ForegroundColor = cor;
+    Console.WriteLine("Negativo");
+    Console.ResetColor();
+}
